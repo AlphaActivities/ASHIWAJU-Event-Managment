d57e65342a146928edfd82bc7050ce52ebcc4412
Josh the Man <gta.alpha.activities@gmail.com>
2026-04-06 12:48:28 -0500

    Updated WorksSection.tsx

--- src/components/sections/WorksSection.tsx
+++ src/components/sections/WorksSection.tsx
@@ -12,7 +12,7 @@ const WORK_IMAGES = [
   "/images/work/work-08.jpg",
   "/images/work/work-16.jpg",
   "/images/work/work-10.jpg",
-  "/images/work/work-12.jpg",
+  "/images/work/work-17.jpg",
   "/images/work/work-15.jpg"
 ];
 
