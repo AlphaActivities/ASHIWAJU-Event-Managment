f5791a07c9149b6a9b9ae57f1102235fd87e55bd
Josh the Man <gta.alpha.activities@gmail.com>
2026-04-07 08:29:10 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/sections/HeroSection';
 import UnderstandingSection from './components/sections/UnderstandingSection';
 import AboutSection from './components/sections/AboutSection';
-import ServicesSection from './components/sections/ServicesSection';
 import TestimonialsSection from './components/sections/TestimonialsSection';
 import WorksSection from './components/sections/WorksSection';
 import FearSection from './components/sections/FearSection';
